71f04abd70bc65ff21861a7bd50ebcafe6c83f68
MQG <mo1056610188@163.com>
2018-09-18 00:08:36 +0800

    MapMessage

--- src/main/java/com/momo/springbootactivemq/queue/origin/QueueReceiver.java
+++ src/main/java/com/momo/springbootactivemq/queue/origin/QueueReceiver.java
@@ -3,6 +3,7 @@ package com.momo.springbootactivemq.queue.origin;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.util.Enumeration;
 
 public class QueueReceiver {
 
@@ -12,19 +13,51 @@ public class QueueReceiver {
         Connection connection = connectionFactory.createConnection();
         connection.start();
 
+        // JMSXPropertyNames
+        getJMSXProperty(connection);
+
         final Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
         Destination destination = session.createQueue("origin.queue");
 
         MessageConsumer consumer = session.createConsumer(destination);
+
+        // TextMessage
+//        receiveTextMessage(session, consumer);
+        // MapMessage
+        receiveMapMessage(session, consumer);
+
+        session.close();
+        connection.close();
+    }
+
+    private static void getJMSXProperty(Connection connection) throws Exception {
+        Enumeration jmsxPropertyNames = connection.getMetaData().getJMSXPropertyNames();
+        while (jmsxPropertyNames.hasMoreElements()) {
+            String name = (String) jmsxPropertyNames.nextElement();
+            System.out.println("jmsx name=" + name);
+        }
+    }
+
+    private static void receiveTextMessage(Session session, MessageConsumer consumer) throws Exception {
         int i = 0;
         while (i < 3) {
             i++;
             TextMessage message = (TextMessage) consumer.receive();
             session.commit();
-            System.out.println("收到消息：" + message.getText());
+            System.out.println("收到TextMessage：" + message.getText());
         }
+    }
 
-        session.close();
-        connection.close();
+    private static void receiveMapMessage(Session session, MessageConsumer consumer) throws Exception {
+        int i = 0;
+        while (i < 3) {
+
+            MapMessage message = (MapMessage) consumer.receive();
+            session.commit();
+            System.out.println("收到MapMessage：" + message.getString("map message"));
+            System.out.println("extra message：" + message.getStringProperty("extra"));
+
+            i++;
+        }
     }
 }
--- src/main/java/com/momo/springbootactivemq/queue/origin/QueueSender.java
+++ src/main/java/com/momo/springbootactivemq/queue/origin/QueueSender.java
@@ -18,15 +18,34 @@ public class QueueSender {
         Destination destination = session.createQueue("origin.queue");
 
         MessageProducer producer = session.createProducer(destination);
+
+        // text消息
+//        sendTextMessage(session, producer);
+        // map消息
+        sendMapMessage(session, producer);
+
+        session.commit();
+        session.close();
+        connection.close();
+    }
+
+    private static void sendTextMessage(Session session, MessageProducer producer) throws Exception {
         for (int i = 0; i < 3; i++) {
             TextMessage message = session.createTextMessage("发送消息，i=" + i);
             Thread.sleep(1000);
             // 生产者发送消息
             producer.send(message);
         }
+    }
 
-        session.commit();
-        session.close();
-        connection.close();
+    private static void sendMapMessage(Session session, MessageProducer producer) throws Exception {
+        for (int i = 0; i < 3; i++) {
+            MapMessage message = session.createMapMessage();
+            message.setString("map message", "map message " + i);
+            message.setStringProperty("extra", "extra message " + i);
+
+            // 生产者发送消息
+            producer.send(message);
+        }
     }
 }
